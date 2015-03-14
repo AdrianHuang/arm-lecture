f010120c5b98cfffa90af6bfb3779334a7d0bf7a
Adrian Huang <adrianhuang0701@gmail.com>
2015-03-14 18:06:36 +0800

    fibonacci: iterative approach
    This commit also tries to minize the code by means of the conditional
    code.

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -15,19 +15,33 @@ fibonacci:
 
 	@ R4 = R0 - 0 (update flags)
 	@ if(R0 <= 0) goto .L3 (which returns 0)
+	subs r4, r0, #1
+	ble .L3
 
 	@ Compare R4 wtih 1
 	@ If R4 == 1 goto .L4 (which returns 1)
+	cmp r4, #1
+	beq .L4
 
-	@ R0 = R4 - 1
-	@ Recursive call to fibonacci with R4 - 1 as parameter
+	@ R3: index
+	@ R4: f(n)
+	@ R5: f(n-1)
+	mov r3, #2
+	mov r4, #1
+	mov r5, #0
 
-	@ R5 = R0
-	@ R0 = R4 - 2
-	@ Recursive call to fibonacci with R4 - 2 as parameter
+loop:
+	@ f(n) = f(n-1) + f(n-2)
+	add r4, r4, r5
 
-	@ R0 = R5 + R0 (update flags)
+	@ Make f(n-1) = f(n-2)
+	sub r5, r4, r5
 
+	add r3, r3, #1
+	cmp r3, r0
+	ble loop
+
+	mov r0, r4
 	pop {r3, r4, r5, pc}		@EPILOG
 
 	@ END CODE MODIFICATION
