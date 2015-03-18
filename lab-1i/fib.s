ffaf4a2c6a5a58212fe45eb5bc5e71b7f7d2a706
Adrian Huang <adrianhuang0701@gmail.com>
2015-03-18 21:04:29 +0800

    Change the loop index from i=0 to i=n
    Per the suggestion of the section 5.3 C Looping Structures of the
    ARM System Developer's Guide, loop index with a fixed number of
    interations should be started from i=n (decrement the loop index
    for each iteration). This approch can save one instruction comparing
    to the iteration with incrementing the loop index.
    
    BTW, this commit also eliminates the unused register 'r3'.

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -11,22 +11,17 @@
 fibonacci:
 	@ ADD/MODIFY CODE BELOW
 	@ PROLOG
-	push {r3, r4, r5, lr}
+	push {r4, r5, lr}
 
 	@ R4 = R0 - 0 (update flags)
 	@ if(R0 <= 0) goto .L3 (which returns 0)
 	subs r4, r0, #1
 	ble .L3
 
-	@ Compare R4 wtih 1
-	@ If R4 == 1 goto .L4 (which returns 1)
-	cmp r4, #1
-	beq .L4
-
-	@ R3: index
+	@ R0: index (The starting index is n-1)
 	@ R4: f(n)
 	@ R5: f(n-1)
-	mov r3, #2
+	sub r0, r0, #1
 	mov r4, #1
 	mov r5, #0
 
@@ -37,21 +32,16 @@ loop:
 	@ Make f(n-1) = f(n-2)
 	sub r5, r4, r5
 
-	add r3, r3, #1
-	cmp r3, r0
-	ble loop
+	subs r0, r0, #1
+	bne loop
 
 	mov r0, r4
-	pop {r3, r4, r5, pc}		@EPILOG
+	pop {r4, r5, pc}		@EPILOG
 
 	@ END CODE MODIFICATION
 .L3:
 	mov r0, #0			@ R0 = 0
-	pop {r3, r4, r5, pc}		@ EPILOG
-
-.L4:
-	mov r0, #1			@ R0 = 1
-	pop {r3, r4, r5, pc}		@ EPILOG
+	pop {r4, r5, pc}		@ EPILOG
 
 	.size fibonacci, .-fibonacci
 	.end
